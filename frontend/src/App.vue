6564c47754373e42b151c4ec8f0d81c2c2e9b1c7
Lester James V. Miranda <ljvmiranda@gmail.com>
2020-04-16 23:12:38 +0800

    Remove HelloWorld components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GenerateButton msg="Hi there"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GenerateButton from './components/GenerateButton.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GenerateButton
   }
 }
 </script>
