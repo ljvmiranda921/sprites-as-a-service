15193d35d20f431c1c44f8f5bc803418c264b72c
Lester James V. Miranda <ljvmiranda@gmail.com>
2020-04-16 23:12:55 +0800

    Implement Generate button to display sprites (#18)
    Resolves #18

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,42 @@
 <template>
   <div id="app">
-    <GenerateButton msg="Hi there"/>
+    <GenerateButton @create-sprite="setSprite($event)" />
+    <SpriteImage :img="img" />
   </div>
 </template>
 
 <script>
-import GenerateButton from './components/GenerateButton.vue'
+import GenerateButton from "./components/GenerateButton.vue";
+import SpriteImage from "./components/SpriteImage.vue";
+import axios from "axios";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    GenerateButton
+    GenerateButton,
+    SpriteImage
+  },
+  data: function() {
+    return {
+      img: ""
+    };
+  },
+  methods: {
+    setSprite: function(value) {
+      this.img = value;
+    }
+  },
+  created: function() {
+    // Random sprite image is called every reload
+    axios
+      .get("http://localhost:8000/sprite")
+      .then(response => {
+        console.log("response", response.statusText);
+        this.img = response.data;
+      })
+      .catch(error => console.log("error.response", error));
   }
-}
+};
 </script>
 
 <style>
