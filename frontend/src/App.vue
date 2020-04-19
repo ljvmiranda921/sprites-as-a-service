5c3674fefc19429653a48d9cbdccd694ca71a1f0
Lester James V. Miranda <ljvmiranda@gmail.com>
2020-04-19 11:32:29 +0800

    Add a display component for cURL snippets (#25)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,23 +2,27 @@
   <div id="app">
     <SpriteDisplay :img="img" />
     <SpriteController @update-sprite="generateSprite($event)" />
+    <SnippetDisplay :url="requesturl" />
   </div>
 </template>
 
 <script>
 import SpriteDisplay from "./components/SpriteDisplay.vue";
 import SpriteController from "./components/SpriteController";
+import SnippetDisplay from "./components/SnippetDisplay";
 import axios from "axios";
 
 export default {
   name: "App",
   components: {
     SpriteDisplay,
-    SpriteController
+    SpriteController,
+    SnippetDisplay
   },
   data() {
     return {
       img: "",
+      requesturl: "",
       baseurl: "http://localhost:8000/sprite",
       spriteConfig: {
         q: null,
@@ -28,14 +32,21 @@ export default {
     };
   },
   methods: {
-    setSprite(value) {
-      this.img = value;
-    },
     generateSprite(spriteConfig) {
+      // Whether spriteConfig.q is null or an empty string,
+      // we will always return null
+      var queryPrefix = "?q=";
+      var extRatePrefix = "&ext_rate=";
+      var queryText;
+      if (spriteConfig.q === null || spriteConfig.q == "") {
+        queryPrefix = "";
+        extRatePrefix = "?ext_rate=";
+        queryText = null
+      }
       axios
         .get(this.baseurl, {
           params: {
-            q: spriteConfig.q,
+            q: queryText,
             ext_rate: spriteConfig.extRate,
             stasis_rate: spriteConfig.stasisRate,
             size: 300
@@ -44,6 +55,11 @@ export default {
         .then(response => {
           console.log("response", response.statusText);
           this.img = response.data;
+          this.requesturl = (this.baseurl
+                          + queryPrefix + spriteConfig.q
+                          + extRatePrefix + spriteConfig.extRate
+                          + "&stasis_rate=" + spriteConfig.stasisRate
+                          + "&size=" + 300);
         })
         .catch(error => console.log("error.response", error));
     }
@@ -59,6 +75,7 @@ export default {
       .then(response => {
         console.log("response", response.statusText);
         this.img = response.data;
+        this.requesturl = this.baseurl;
       })
       .catch(error => console.log("error.response", error));
   }
---
+++ frontend/src/components/SnippetDisplay.vue
@@ -0,0 +1,16 @@
+<template>
+  <div> 
+    <p>curl -X GET {{ url }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["url"]
+}
+</script>
+
+
+<style scoped>
+
+</style>
