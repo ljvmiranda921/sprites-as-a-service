12faeb4e0616492f6f69698f5da968231eec258c
Lester James V. Miranda <ljvmiranda@gmail.com>
2020-04-25 20:58:29 +0800

    chore: Autoformat using prettier

--- frontend/src/components/TechnicalNotesLicensing.vue
+++ frontend/src/components/TechnicalNotesLicensing.vue
@@ -3,12 +3,13 @@
     <h3>Cool stuff! Can I use this for my game or app?</h3>
     <p>
       Of course! Sprite-as-a-Service is dual-licensed: the source code is MIT,
-      while the generated sprites are under the Creative Commons Attribution
-      4.0 (CC BY 4.0). However, please support me and this work by:
+      while the generated sprites are under the Creative Commons Attribution 4.0
+      (CC BY 4.0). However, please support me and this work by:
     </p>
     <ul>
-      <li>Attributing and notifying me on Github when you're using this
-        service.</li>
+      <li>
+        Attributing and notifying me on Github when you're using this service.
+      </li>
       <li>Contributing back and leaving a star on Github!</li>
       <li>
         Helping me maintain server costs, buy me a coffee!
