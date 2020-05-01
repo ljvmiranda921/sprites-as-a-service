abd48efd98176401e73cdb6f2ce1e09a6939d4ea
Lester James V. Miranda <ljvmiranda@gmail.com>
2020-05-01 20:30:57 +0800

    [wip] Update

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import App from "./App.vue";
 require("pattern.css/dist/pattern.min.css");
 require("shorthandcss/dist/shorthand.min.css");
 require("nes.css/css/nes.min.css");
+require("@fortawesome/fontawesome-free/css/all.min.css");
 
 Vue.config.productionTip = false;
 
