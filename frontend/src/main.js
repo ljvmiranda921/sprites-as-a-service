65c7561a7db2ed5b9a7729a9ec59ff5818fb7fc2
Lester James V. Miranda <ljvmiranda@gmail.com>
2020-05-01 08:40:36 +0800

    fixup

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 
+require('pattern.css/dist/pattern.min.css')
+require('shorthandcss/dist/shorthand.min.css')
+
 Vue.config.productionTip = false;
 
 new Vue({
