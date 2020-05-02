e78ef34408d13ff6f8f8788faf022d0c00f8dc3a
Lester James V. Miranda <ljvmiranda@gmail.com>
2020-05-02 17:37:28 +0800

    Update CORS

--- backend/sprites/main.py
+++ backend/sprites/main.py
@@ -25,7 +25,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080", "http://localhost"],
+    allow_origins=["http://localhost:3000", "http://localhost"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
