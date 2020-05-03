c5558e52b35e3cd2804e9d4f129f89dbd871da36
Lester James V. Miranda <ljvmiranda@gmail.com>
2020-05-03 14:58:50 +0800

    Add external link to documentation

--- frontend/components/SnippetDisplayCTA.vue
+++ frontend/components/SnippetDisplayCTA.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h4>Read more in the documentation</h4>
+    <h4>Read more in the <a href="https://sprites-as-a-service-tblytwilzq-ue.a.run.app/redoc">documentation</a></h4>
   </div>
 </template>
 
