9e2288981b6ae88294c23e975ea44eb4138e1199
Lester James V. Miranda <ljvmiranda@gmail.com>
2020-05-09 22:16:57 +0800

    Add social meta tags
    This is so that it looks nice when shared in social media. As of now it
    just gives us the link.
    
    Signed-off-by: Lester James V. Miranda <ljvmiranda@gmail.com>

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -3,8 +3,8 @@ const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
     base: '/sprites-as-a-service/'
   }
 } : {}
-
 const favicon = process.env.DEPLOY_ENV === 'GH_PAGES' ? '/sprites-as-a-service/favicon.ico' : '/favicon.ico'
+const header = process.env.DEPLOY_ENV === 'GH_PAGES' ? '/sprites-as-a-service/header.png' : '/header.png'
 
 export default {
   ...routerBase,
@@ -23,7 +23,16 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      /* Add social links for Facebook and Twitter */
+      { hid: 'og:description', name: 'og:description', content: process.env.npm_package_description || '' },
+      { hid: 'og:image', name: 'og:image', content: header },
+      { hid: 'og:width', name: 'og:width', content: '474' },
+      { hid: 'og:height', name: 'og:height', content: '905' },
+      { hid: 'twitter:image', name: 'twitter:image', content: header },
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
+      { hid: 'twitter:title', name: 'twitter:title', content: process.env.npm_package_name || '' },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@ljvmiranda921' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: favicon }
